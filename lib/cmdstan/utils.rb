f180b3a8498cfd98748178d9c97158ed172c4c07
Andrew Kane <andrew@chartkick.com>
2020-04-10 00:57:35 -0700

    Live output

--- lib/cmdstan/utils.rb
+++ lib/cmdstan/utils.rb
@@ -3,13 +3,7 @@ module CmdStan
     private
 
     def run_command(*args)
-      # use an open3 method since it does escaping (like system)
-      # use capture2e so we don't need to worry about deadlocks
-      output, status = Open3.capture2e(*args)
-      if status.exitstatus != 0
-        $stderr.puts output
-        raise Error, "Command failed"
-      end
+      system *args
     end
 
     def make_command
